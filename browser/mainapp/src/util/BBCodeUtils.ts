5efdda0603e4bef4e103bf71d5688df37388d14e
Xariah Dailstone <xariah@xariah.net>
2025-01-19 02:09:11 -0500

    Add BBCode editing keyboard shortcuts to status edit dialog (#58)

--- browser/mainapp/src/util/BBCodeUtils.ts
+++ browser/mainapp/src/util/BBCodeUtils.ts
@@ -1,6 +1,91 @@
+import { AppViewModel } from "../viewmodel/AppViewModel";
+import { EIconSearchDialogViewModel } from "../viewmodel/dialogs/EIconSearchDialogViewModel";
 import { ChatBBCodeParser } from "./bbcode/BBCode";
+import { KeyCodes } from "./KeyCodes";
+import { TextEditShortcutsHelper } from "./TextEditShortcutsHelper";
 
 const urlPattern = new RegExp(/(.*?)(http(s)?\:\/\/(\S+))/, "ig");
+
+function tryHandleEditShortcutKey(textarea: HTMLTextAreaElement, ev: KeyboardEvent, options: AddEditingShortcutsOptions) {
+    if (ev.ctrlKey) {
+        const tesh = new TextEditShortcutsHelper();
+        tesh.value = textarea.value;
+        tesh.selectionAt = Math.min(textarea.selectionStart, textarea.selectionEnd)
+        tesh.selectionLength = Math.abs(textarea.selectionEnd - textarea.selectionStart);
+
+        let loadBack = false;
+
+        switch (ev.keyCode) {
+            case KeyCodes.KEY_B:
+                tesh.bold();
+                loadBack = true;
+                break;
+            case KeyCodes.KEY_E:
+                // tesh.eicon();
+                const avm = options.appViewModelGetter();
+                if (avm) {
+                    loadBack = true;
+                    (async () => {
+                        const pdialog = new EIconSearchDialogViewModel(avm);
+                        const dlgResult = await avm.showDialogAsync(pdialog);
+                        if (dlgResult) {
+                            tesh.eicon(dlgResult);
+                            textarea.value = tesh.value;
+                            textarea.setSelectionRange(tesh.selectionAt, tesh.selectionAt + tesh.selectionLength);
+                            options.onTextChanged(textarea.value);
+                        }
+                        textarea.focus();
+                    })();
+                }
+                break;
+            case KeyCodes.KEY_I:
+                tesh.italic();
+                loadBack = true;
+                break;
+            case KeyCodes.KEY_U:
+                tesh.underline();
+                loadBack = true;
+                break;
+            case KeyCodes.UP_ARROW:
+                tesh.superscript();
+                loadBack = true;
+                break;
+            case KeyCodes.DOWN_ARROW:
+                tesh.subscript();
+                loadBack = true;
+                break;
+            case KeyCodes.KEY_K:
+                tesh.spoiler();
+                loadBack = true;
+                break;
+            case KeyCodes.KEY_N:
+                tesh.noparse();
+                loadBack = true;
+                break;
+            case KeyCodes.KEY_O:
+                tesh.icon();
+                loadBack = true;
+                break;
+            case KeyCodes.KEY_R:
+                tesh.user();
+                loadBack = true;
+                break;
+            case KeyCodes.KEY_S:
+                tesh.strikethrough();
+                loadBack = true;
+                break;
+        }
+
+        if (loadBack) {
+            textarea.value = tesh.value;
+            textarea.setSelectionRange(tesh.selectionAt, tesh.selectionAt + tesh.selectionLength);
+            options.onTextChanged(textarea.value);
+            return true;
+        }
+    }
+
+    return false;
+}
 
 export class BBCodeUtils {
     static autoWrapUrls(str: string): string {
@@ -56,6 +141,48 @@ export class BBCodeUtils {
         }
         return finalResultBuilder.join("");
     }
+
+    static addEditingShortcuts(textarea: HTMLTextAreaElement, options: AddEditingShortcutsOptions) {
+        if (!(options?.onKeyDownHandler)) {
+            options.onKeyDownHandler = (ev, handleShortcuts) => {
+                handleShortcuts(ev);
+            }
+        }
+        else {
+            textarea.addEventListener("keydown", (ev) => {
+                options.onKeyDownHandler!(ev, (ev) => {
+                    if (tryHandleEditShortcutKey(textarea, ev, options)) {
+                        ev.preventDefault();
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                });
+            });
+        }
+        textarea.addEventListener("paste", (ev: ClipboardEvent) => {
+            let pasteText = ev.clipboardData?.getData("text") ?? "";
+            pasteText = BBCodeUtils.autoWrapUrls(pasteText);
+            
+            const selStart = textarea.selectionStart;
+            const selEnd = textarea.selectionEnd;
+            let v = textarea.value.substring(0, selStart) +
+                pasteText +
+                textarea.value.substring(selEnd);
+            document.execCommand("insertText", false, pasteText);
+            textarea.setSelectionRange(selStart + pasteText.length, selStart + pasteText.length, "forward");
+            options.onTextChanged(textarea.value);
+
+            ev.preventDefault();
+        });
+    }
+}
+
+interface AddEditingShortcutsOptions {
+    appViewModelGetter: () => AppViewModel | null,
+    onKeyDownHandler?: (ev: KeyboardEvent, handleShortcuts: (ev: KeyboardEvent) => boolean) => void;
+    onTextChanged: (value: string) => void;
 }
 
 (window as any)["__bbcodeutils"] = BBCodeUtils;
