958465935247996a62ccb30a0e9d0bebec5742fd
Xariah Dailstone <xariah@xariah.net>
2025-12-05 16:04:11 -0500

    More unified UI experiments

---
+++ host/win32/XarChat.UI.Win32/Class1.cs
@@ -0,0 +1,30 @@
+﻿using XarChat.UI.Abstractions;
+
+namespace XarChat.UI.Win32
+{
+    public class Win32Application : IApplication
+    {
+        public bool SupportsMultipleWindows => true;
+
+        public SynchronizationContext SynchronizationContext => throw new NotImplementedException();
+
+        public bool NeedInvoke => throw new NotImplementedException();
+
+        public event EventHandler<EventArgs>? OnRunning;
+
+        public IWebViewWindow CreateWebViewWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
